fb45ec2458838c6a47b8f05844c42acb41b233ae
yweizhang <zhang2486@wisc.edu>
2022-09-20 17:08:04 -0500

    f

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py --coco_path ./coco --pix2seq_lr --large_scale_jitter --rand_target $@
+python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --coco_path /nobackup/yiwei/coco/images --pix2seq_lr --large_scale_jitter --rand_target $@ --batch_size 16 --epochs 50
