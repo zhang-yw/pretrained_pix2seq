120433a455023ef777bad4e841d08426906c358f
yweizhang <zhang2486@wisc.edu>
2022-09-21 01:24:41 -0500

    checkpoint epochs

--- main.py
+++ main.py
@@ -214,7 +214,7 @@ def main(args):
         if args.output_dir:
             checkpoint_paths = [output_dir / 'checkpoint.pth']
             # extra checkpoint before LR drop and every 100 epochs
-            if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % 100 == 0:
+            if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % 5 == 0:
                 checkpoint_paths.append(output_dir / f'checkpoint{epoch:04}.pth')
             if cur_ap > max_ap:
                 checkpoint_paths.append(output_dir / 'checkpoint_best.pth')
