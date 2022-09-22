f618e8397907a6007832fbeb7f28ac5a6cdf6f6e
yweizhang <zhang2486@wisc.edu>
2022-09-22 18:04:31 -0500

    f

--- playground/pix2seq/pix2seq.py
+++ playground/pix2seq/pix2seq.py
@@ -13,7 +13,7 @@ from util.misc import (NestedTensor, nested_tensor_from_tensor_list,
 from .backbone import build_backbone
 from .transformer import build_transformer
 from util.box_ops import box_cxcywh_to_xyxy
-import numpy as np
+# import numpy as np
 
 class Pix2Seq(nn.Module):
     """ This is the Pix2Seq module that performs object detection """
@@ -182,19 +182,19 @@ class SetCriterion(nn.Module):
         a1  = 1
         b1  = (height + width)
         c1  = width * height * (1 - min_overlap) / (1 + min_overlap)
-        sq1 = np.sqrt(b1 ** 2 - 4 * a1 * c1)
+        sq1 = torch.sqrt(b1 ** 2 - 4 * a1 * c1)
         r1  = (b1 + sq1) / 2
 
         a2  = 4
         b2  = 2 * (height + width)
         c2  = (1 - min_overlap) * width * height
-        sq2 = np.sqrt(b2 ** 2 - 4 * a2 * c2)
+        sq2 = torch.sqrt(b2 ** 2 - 4 * a2 * c2)
         r2  = (b2 + sq2) / 2
 
         a3  = 4 * min_overlap
         b3  = -2 * min_overlap * (height + width)
         c3  = (min_overlap - 1) * width * height
-        sq3 = np.sqrt(b3 ** 2 - 4 * a3 * c3)
+        sq3 = torch.sqrt(b3 ** 2 - 4 * a3 * c3)
         r3  = (b3 + sq3) / 2
         return torch.minimum(r1, r2, r3)
 
