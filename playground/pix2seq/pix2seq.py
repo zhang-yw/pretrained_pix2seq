d669ec70f91e6e8b56145f1ddd9a5984efc4ac65
yweizhang <zhang2486@wisc.edu>
2022-09-23 02:25:40 -0500

    f

--- playground/pix2seq/pix2seq.py
+++ playground/pix2seq/pix2seq.py
@@ -235,6 +235,8 @@ class SetCriterion(nn.Module):
                     radius = radius_arr[object]
                     radius = max(0, radius.int())
                     diameter = 2 * radius + 1
+                    print(radius)
+                    exit(0)
                     # diameter = diameter.cpu().numpy()
                     gaussian = self.gaussian1D(diameter, sigma=diameter / 6)
                     # gaussian = torch.from_numpy(gaussian)
