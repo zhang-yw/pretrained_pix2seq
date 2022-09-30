e60b6ca3dff9fdc5cb6f3427c00f2f142833aa42
yweizhang <zhang2486@wisc.edu>
2022-09-29 20:48:50 -0500

    d

--- playground/pix2seq/pix2seq.py
+++ playground/pix2seq/pix2seq.py
@@ -365,7 +365,9 @@ class SetCriterion(nn.Module):
         # print(pred_seq_logits.shape)
         # print(target_seq.shape)
         # exit(0)
-
+        print(pred_seq_logits[0])
+        print(pred_seq_logits[0].sum())
+        exit()
         # self.empty_weight[0:self.num_bins+1] = 0.
         empty_weight = torch.ones(self.num_vocal).to(self.device)
         empty_weight[-1] = self.eos_coef
